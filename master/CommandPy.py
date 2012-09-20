c3182cc35d2301cdbe3f8aed5f2eaae4e520bd28
itdxer <yurayu92@gmail.com>
2012-09-20 20:31:56 +0300

    PigShell class
    PigShell class extends class CommandPy

--- master/CommandPy.py
+++ master/CommandPy.py
@@ -1,7 +1,4 @@
-from subprocess import Popen, PIPE#, STDOUT
-
-#RUBY_SHELL_PATH = 'C:/Ruby193/bin/ruby C:/Users/Yuri/Dropbox/Programming/Python/Ruby_Python/slave.rb'
-PIG_LATIN_SHELL = 'pig -x local pig.pig'
+from subprocess import Popen, PIPE
 
 class CommandPy:
 
@@ -20,7 +17,7 @@ class CommandPy:
         slave = Popen(self.shell_path, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds = True)
         answer = slave.communicate()
         if not answer[1]:
-            return answer[0]#.split(self.separator)\
+            return answer[0]
         else:
             self.last_error = answer[1]
             return False
@@ -30,20 +27,4 @@ class CommandPy:
         #fl.writelines(self.separator)
         fl.writelines(command)
         fl.close()
-
-
-pig = CommandPy(PIG_LATIN_SHELL)
-command = []
-
-while True:
-    enter_command = raw_input(">>")
-    if enter_command == 'quite':
-        break
-    command.append(enter_command)
-    code = pig.returnCode(command)
-    if code:
-        print code
-    else:
-        print pig.last_error
-    command = []
 
