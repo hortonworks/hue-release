1f15754306e02a6e917406a31859100e7865594b
Alexander Burdeinyj <Bonus_05@mail.ru>
2012-09-27 19:35:56 +0300

    changed the way of static files connection in pig; correted css scheme; added PIG.README file; some changes for SANDBOX-17; added apache license header into all files;

--- master/pig/src/pig/views.py
+++ master/pig/src/pig/views.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
-# Licensed to Cloudera, Inc. under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  Cloudera, Inc. licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+## Licensed to the Apache Software Foundation (ASF) under one
+## or more contributor license agreements.  See the NOTICE file
+## distributed with this work for additional information
+## regarding copyright ownership.  The ASF licenses this file
+## to you under the Apache License, Version 2.0 (the
+## "License"); you may not use this file except in compliance
+## with the License.  You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing,
+## software distributed under the License is distributed on an
+## "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+## KIND, either express or implied.  See the License for the
+## specific language governing permissions and limitations
+## under the License.
 
 from desktop.lib.django_util import render
 
@@ -67,19 +67,15 @@ def one_script(request, obj_id, text = False):
                 f1.close()
                 pig = CommandPy('pig -x local pig.pig')
                 text = pig.returnCode() or pig.last_error
-
+            if request.POST.get('submit') in ['Explain', 'Describe', 'Dump']:
+                command = request.POST.get('submit').upper()
+                f1 = open('pig.pig', 'w')
+                f1.write(instance[0].text)
+                f1.close()
+                pig = PigShell('pig -x local pig.pig')
+                text = pig.ShowCommands(command=command) or pig.last_error
     form = PigScriptForm(instance.values('title', 'text')[0])
     return render('edit_script.mako', request, dict(form=form, instance=instance[0], pig_script=pig_script, text=text))
-
-def command(request, obj_id, command):
-    instance = PigScript.objects.get(id=obj_id)
-    f1 = open('pig.pig', 'w')
-    f1.write(instance.text)
-    f1.close()
-    pig = PigShell('pig -x local pig.pig')
-    pig_answer = pig.ShowCommands(command=command) or pig.last_error
-    return one_script(request, obj_id, text=pig_answer)
-
 
 
 def delete(request, obj_id):
