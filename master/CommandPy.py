89340163673d9c4edfbd9be2187a0c89e5940b02
Yura Shevchuk <yurayu92@gmail.com>
2012-10-01 15:39:55 +0300

    add logs

--- master/CommandPy.py
+++ master/CommandPy.py
@@ -1,19 +1,38 @@
 from subprocess import Popen, PIPE
 
+def logs(returnCode):
+    def wrappen(self):
+        from time import strftime
+        start_time = strftime("%Y-%m-%d %H:%M:%S")
+        answer = returnCode(self)
+        end_time = strftime("%Y-%m-%d %H:%M:%S")
+        if not answer:
+            status = '0'
+            answer = self.last_error
+        else:
+            status = '1'
+        log = self.LogModel(start_time = start_time,
+                            end_time = end_time,
+                            status = status,
+                            script_name = self.file_path)
+        log.save()
+        return answer
+    return wrappen
+
 class CommandPy:
 
     file_path = ''
     shell_path = ''
     last_error = ''
-    separator = '\r\n'
 
-    def __init__(self, shell_path):
+    def __init__(self, shell_path, LogModel = None):
         self.shell_path = shell_path.split()
         self.file_path = self.shell_path[-1]
+        self.LogModel = LogModel
 
-    def returnCode(self, code = ''):
-        if code:
-            self.command(code)
+    @logs
+    def returnCode(self):
+
         slave = Popen(self.shell_path, stdin=PIPE, stdout=PIPE,
                       stderr=PIPE, close_fds = True)
         slave.wait()
@@ -26,10 +45,3 @@ class CommandPy:
             return False
         else:
             return answer
-
-    def command(self, command):
-        fl = open(self.file_path, 'a')
-        #fl.writelines(self.separator)
-        fl.writelines(command)
-        fl.close()
-
