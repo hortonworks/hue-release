9795adccd4a405727947353197b33c29cd5d1b24
Yura Shevchuk <yurayu92@gmail.com>
2012-10-10 19:24:32 +0300

    bash script update

--- master/tutorials/tutorials_app/run/run.sh
+++ master/tutorials/tutorials_app/run/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 RUN_DIR=/home/roma/Projects/hdps/tutorials/tutorials_app/run
+GIT_REPO=ssh://git@bitbucket.org/antigluk/lessons.git
+
 cd $RUN_DIR
 
 cd git_files
+[ ! -d .git ] && git init && git remote add origin $GIT_REPO
 echo -n "Pull...  "
 git pull origin master >/dev/null 2>&1
 echo "Done"
