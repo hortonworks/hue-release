4455720104910b20ad49d81745f8c16ab89e241f
Yura Shevchuk <yurayu92@gmail.com>
2012-10-23 20:23:31 +0300

    fix run shell file bugs

--- master/tutorials/tutorials_app/run/run.sh
+++ master/tutorials/tutorials_app/run/run.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-RUN_DIR=/home/roma/Projects/hdps/tutorials/tutorials_app/run
-GIT_REPO=ssh://git@bitbucket.org/antigluk/lessons.git
+RUN_DIR=/home/tutorials/tutorials_app/run
+GIT_REPO=git@github.com:hortonworks/sandbox-shared.git
 
 cd $RUN_DIR
 
-cd git_files
-[ ! -d .git ] && git init && git remote add origin $GIT_REPO
+[ -d git_files_all ] || mkdir git_files_all
+chown hue git_files_all
+cd git_files_all
+CUR_DIR="`pwd`"
+[ ! -d .git ] && su hue -c "cd $CUR_DIR && git init && git remote add origin $GIT_REPO"
 echo -n "Pull...  "
-git pull origin master >/dev/null 2>&1
+su hue -c "cd $CUR_DIR && git pull origin master" # >/dev/null 2>&1
 echo "Done"
 cd - >/dev/null
+cd $RUN_DIR
+[ ! -L git_files ] && ln -s git_files_all/tutorials git_files
+
 echo -n "Updating DB...  "
-python run.py >/dev/null
+/home/tutorials/.env/bin/python run.py >/dev/null
 echo "Done"
