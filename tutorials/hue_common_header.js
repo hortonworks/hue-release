7f7f458278ef97c2def1fefe0582d9587e48588d
Roman Rader <antigluk@gmail.com>
2012-11-23 18:03:47 +0200

    auto-login added to tutorials manual

--- tutorials/hue_common_header.js
+++ tutorials/hue_common_header.js
@@ -1,34 +1,41 @@
-// auto login and anonymous uses
+// auto login and anonymous users
 
-// change drop-down menu for anonymous user
-var isAnonymous = document.getElementById("usernameDropdown").innerText.trim()=="AnonymousUser";
-if (isAnonymous) {
-    var profileRef = document.getElementsByClassName("userProfile")[0];
-    profileRef.innerText = "Create new user";
-    profileRef.href="/useradmin/users/new";
-}
+function handleAutoLogin() {
 
-var isAddingUser = document.location.pathname=="/useradmin/users/new";
-if (isAddingUser && isAnonymous) {
-    document.getElementById("editForm").onsubmit = function(){
-        $.cookie("added","1");
-        return true;
+    // change drop-down menu for anonymous user
+    var isAnonymous = document.getElementById("usernameDropdown").innerText.trim()=="AnonymousUser";
+    if (isAnonymous) {
+        var profileRef = document.getElementsByClassName("userProfile")[0];
+        profileRef.innerText = "Create new user";
+        profileRef.href="/useradmin/users/new";
     }
 
-    if (document.getElementsByClassName("errorlist").length) > 0 {
-        // remove cookie (error while creating)
+    var isAddingUser = document.location.pathname=="/useradmin/users/new";
+    if (isAddingUser && isAnonymous) {
+        document.getElementById("editForm").onsubmit = function(){
+            $.cookie("added","1");
+            return true;
+        }
+
+        if ((document.getElementsByClassName("errorlist").length) > 0) {
+            // remove cookie (error while creating)
+            document.cookie="added=;expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+        }
+    }
+
+    var isAddedSuccessfully = $.cookie("added") !== null;
+    if (isAddedSuccessfully && isAnonymous) {
         document.cookie="added=;expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+        window.location = "/accounts/logout/";
     }
-}
-
-var isAddedSuccessfully = $.cookie("added") !== null;
-if (isAddedSuccessfully && isAnonymous) {
-    window.location = "/accounts/logout/";
-}
 
 
-// save user steps in tutorials
-if(window.top != window) {
-    document.createElement("img").src = "http://ec2-75-101-213-8.compute-1.amazonaws.com:8888/sync/?loc=" +
-    escape(document.location.href);
+    // save user steps in tutorials
+    if(window.top != window) {
+        // ====== REPLACE URL HERE =====
+        document.createElement("img").src = "http://ec2-75-101-213-8.compute-1.amazonaws.com:8888/sync/?loc=" +
+        escape(document.location.href);
+        // =============================
+    }
+
 }
