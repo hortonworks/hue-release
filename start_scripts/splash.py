b81d8b794709e3441cb3d0a45f23bfa0c54770a1
Roman Rader <antigluk@gmail.com>
2012-12-12 16:35:09 +0200

    splash screen determines IP errors

--- start_scripts/splash.py
+++ start_scripts/splash.py
@@ -1,40 +1,65 @@
 import curses
 import sh
 
-screen = curses.initscr()
-curses.noecho()
-
-H, W = screen.getmaxyx()
-
+screen = None
 HINT_WIDTH = 3
 
-greet_win = screen.subwin(H / 2 - HINT_WIDTH, W, 0, 0)
-greet_win.box()
-greet_win.addstr(1,2,"Hortonworks Data Platform Sandbox")
-greet_win.addstr(2,2,"Version 1.0.4")
-greet_win.addstr(3,2,"http://hortonworks.com")
+def make_greet_window():
+    H, W = screen.getmaxyx()
+    greet_win = screen.subwin(H / 2 - HINT_WIDTH, W, 0, 0)
+    greet_win.box()
+    greet_win.addstr(1,2,"Hortonworks Data Platform Sandbox")
+    greet_win.addstr(2,2,"Version 1.0.4")
+    greet_win.addstr(3,2,"http://hortonworks.com")
 
-ip_win = screen.subwin(H / 2, W, H / 2 - HINT_WIDTH, 0)
-ip_win.box()
-ip = sh.head(
-        sh.awk(
-            sh.getent("ahosts", sh.hostname().strip()),
-            "{print $1}"),
-        n="1").strip()
-
-ip_win.addstr(1,2,"Log in to this sandbox by navigating your browser to")
-ip_win.addstr(2,2,"http://%s/" % ip)
-
-hint_win = screen.subwin(HINT_WIDTH, W, H - HINT_WIDTH, 0)
-hint_win.box()
-hint_win.addstr(1,1,"<Alt+F2> Log in to this virtual machine")
-
-screen.refresh()
-
-while True:
+def make_ip_window():
+    H, W = screen.getmaxyx()
+    ip_win = screen.subwin(H / 2, W, H / 2 - HINT_WIDTH, 0)
+    ip_win.box()
     try:
-        screen.getch()
-    except KeyboardInterrupt:
-        pass
+        ip = sh.head(
+                sh.awk(
+                    sh.getent("ahosts", sh.hostname().strip()),
+                    "{print $1}"),
+                n="1").strip()
+    except sh.ErrorReturnCode:
+        ip_win.addstr(1,2,"===================================")
+        ip_win.addstr(2,2,"You have some connectivity issues!")
+        ip_win.addstr(3,2,"Check VM setup instructions")
+        ip_win.addstr(4,2,"===================================")
+    else:
+        ip_win.addstr(1,2,"Log in to this sandbox by navigating your browser to")
+        ip_win.addstr(2,2,"http://%s/" % ip)
 
-curses.endwin()
+def make_hint_window():
+    H, W = screen.getmaxyx()
+    hint_win = screen.subwin(HINT_WIDTH, W, H - HINT_WIDTH, 0)
+    hint_win.box()
+    hint_win.addstr(1,1,"<Alt+F2> Log in to this virtual machine")
+
+def init_screen():
+    curses.noecho()
+
+    make_greet_window()
+    make_ip_window()
+    make_hint_window()
+
+
+def main():
+    global screen
+    screen = curses.initscr()
+    init_screen()
+
+    screen.refresh()
+
+    while True:
+        try:
+            screen.getch()
+        except KeyboardInterrupt:
+            pass
+
+    curses.endwin()
+
+
+if __name__ == '__main__':
+    main()
