cd944247975a76a6a811ee2dd0b825ea2d81193d
Peter <petro.rudenko@gmail.com>
2013-03-19 00:31:08 +0200

    Comment vagrant plugins as they are yet incompatible with vagrant 1.1

--- hue/apps/beeswax/src/beeswax/management/commands/beeswax_install_examples.py
+++ hue/apps/beeswax/src/beeswax/management/commands/beeswax_install_examples.py
@@ -111,7 +111,7 @@ class Command(NoArgsCommand):
     """
     Setup the sample user
     """
-    USERNAME = 'sample'
+    USERNAME = 'sandbox'
     try:
       user = User.objects.get(username=USERNAME)
     except User.DoesNotExist:
