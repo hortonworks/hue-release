582e2dda631c10148ab900731b6554ebdf32adeb
Roman Rader <antigluk@gmail.com>
2013-04-04 19:52:59 +0300

    vmware exporting script

--- deploy/vagrant/export.sh
+++ deploy/vagrant/export.sh
@@ -1,6 +1,11 @@
-set -x
+#set -x
 set -e
 
 vagrant up
+vagrant halt
 MACHINE="$(VBoxManage list vms | grep `python2 -c "import json; print json.loads(file('.vagrant').read())['active']['default']"` | sed -r 's/\"(.*)\".*/\1/g')"
-VBoxManage export "$MACHINE" --output "$MACHINE".ova --vsys 0 --vendor "HortonWorks" --version "Caterpillar" --product "Sandbox"
+VBoxManage export "$MACHINE" --output "$MACHINE VirtualBox".ova --vsys 0 --vendor "HortonWorks" --version "Caterpillar" --product "Sandbox"
+
+HDDFILE="$(VBoxManage list hdds | grep -e "$MACHINE/" | sed -r "s/.*:[^/]*(.*)/\1/g")"
+cp "$HDDFILE" ./vmware/sandbox.vmdk
+ovftool ./vmware/vmware.vmx "./$MACHINE VMware.ova"
