564a8fcd2d631e40f80bbdcf96cc5e6717acf8b9
Peter <petro.rudenko@gmail.com>
2013-04-08 19:42:48 +0300

    [Beeswax] BUG-4771 IE9 compability

--- hue/apps/hcatalog/src/hcatalog/static/js/visualization.js
+++ hue/apps/hcatalog/src/hcatalog/static/js/visualization.js
@@ -1,8 +1,47 @@
 $(document).ready(function () {
-	  var delay;
-      // Initialize CodeMirror editor with a nice html5 canvas demo.
-
+		var delay;
+		var styles = '<style>';
+		var lib = '<script>';
+		if (jQuery.browser.msie == true) { 
 		
+
+			$.get('/static/ext/js/jquery/jquery-1.8.1.min.js', function(data) {
+				lib += data;
+			});
+			$.get('/hcatalog/static/js/jquery-ui.min.js', function(data) {
+				lib += data;
+			});
+			$.get('/hcatalog/static/js/d3.v3.min.js', function(data) {
+				lib += data;
+			});
+			$.get('/hcatalog/static/js/lib/d3.layout.min.js', function(data) {
+				lib += data;
+			});
+			$.get('/hcatalog/static/js/lib/rickshaw.min.js', function(data) {
+				lib += data;
+			});
+			$.get('/hcatalog/static/js/chart.js', function(data) {
+				lib += data;
+			});
+
+	
+			$.get('/hcatalog/static/css/detail.css', function(data) {
+				styles += data;
+			});
+			$.get('/hcatalog/static/css/graph.css', function(data) {
+				styles += data;
+			});
+			$.get('/hcatalog/static/css/legend.css', function(data) {
+				styles += data;
+			});
+			$.get('/hcatalog/static/css/chart.css', function(data) {
+				styles += data;
+			});
+			$.get('/hcatalog/static/css/jquery-ui.css', function(data) {
+				styles += data;
+			});
+		}
+	  // Initialize CodeMirror editor with a nice html5 canvas demo.
       var editor = CodeMirror.fromTextArea(document.getElementById('vis_code'), {
         mode: 'text/html',
         tabMode: 'indent',
@@ -12,14 +51,22 @@ $(document).ready(function () {
         }
       });
 
+	  var previewFrame = document.getElementById('preview');	
+	  var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
       function updatePreview() {
-        var previewFrame = document.getElementById('preview');
-        var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
-
-        preview.open();
-        preview.write(editor.getValue());
-        preview.close();
+		preview.open();
+		if (jQuery.browser.msie == true) { 
+			styles += '</style>';
+			lib += '</script><div class="chart_wrap"><div id="y_axis"></div><div id="chart"></div><div id="slider"></div><div id="x_axis"></div><div id="legend"></div></div>';
+			preview.write(lib+styles+editor.getValue());
+		}
+		else
+		{
+			var include = '<script src="/hcatalog/static/js/include.js"></script>';
+			preview.write(include+editor.getValue());
+		}
+		preview.close();
       }
-	  $('a[href="#visualizations"]').live('click',function(){updatePreview(); editor.refresh();});
+	  $('a[href="#visualizations"]').live('click',function(){  editor.refresh(); });
       setTimeout(updatePreview, 700);
 });
