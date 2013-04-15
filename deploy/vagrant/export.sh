da121138db6721152d1686bb35f315a65c990a54
Roman Rader <antigluk@gmail.com>
2013-04-15 23:14:55 +0300

    openssl as requirement; fix in hue start script

--- deploy/vagrant/export.sh
+++ deploy/vagrant/export.sh
@@ -9,6 +9,10 @@ VBoxManage modifyvm "$MACHINE" --memory 2048 --cpus 2
 VBoxManage export "$MACHINE" --output "$MACHINE VirtualBox".ova \
     --vsys 0 --vendor "HortonWorks" --version "Caterpillar" --product "Sandbox"
 
+vagrant up --no-provision
+vagrant ssh -c "sudo /opt/VBoxGuestAdditions-*/uninstall.sh"
+vagrant halt
+
 HDDFILE="$(VBoxManage list hdds | grep -e "$MACHINE/" | sed -r "s/.*:[^/]*(.*)/\1/g")"
 cp "$HDDFILE" ./vmware/sandbox.vmdk
 ovftool ./vmware/vmware.vmx "./$MACHINE VMware.ova"
