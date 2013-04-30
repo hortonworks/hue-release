8e9e17d7514d0e7fa99e051c6a8fee709286f1ef
Peter Rudenko <petro.rudenko@gmail.com>
2013-04-30 21:43:33 +0300

    Load videos for tutorials.

---
+++ deploy/vagrant/files/scripts/load_videos.sh
@@ -0,0 +1,4 @@
+wget -O /tmp/videos.tar.gz http://dev2.hortonworks.com.s3.amazonaws.com/videos.tar.gz
+mkdir /usr/lib/tutorials/sandbox-tutorials/file
+tar xvf /tmp/videos.tar.gz -C /usr/lib/tutorials/sandbox-tutorials/file
+exit 0
