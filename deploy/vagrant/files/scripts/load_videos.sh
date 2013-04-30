aa8c66dc363a75b3f728d2838b3fbf5c310b41be
Roman Rader <antigluk@gmail.com>
2013-04-30 22:24:15 +0300

    load_videos path fixed

--- deploy/vagrant/files/scripts/load_videos.sh
+++ deploy/vagrant/files/scripts/load_videos.sh
@@ -1,4 +1,9 @@
 wget -O /tmp/videos.tar.gz http://dev2.hortonworks.com.s3.amazonaws.com/videos.tar.gz
-mkdir /usr/lib/tutorials/sandbox-tutorials/file
-tar xvf /tmp/videos.tar.gz -C /usr/lib/tutorials/sandbox-tutorials/file
+mkdir /usr/lib/tutorials/sandbox-tutorials
+tar xvf /tmp/videos.tar.gz -C /usr/lib/tutorials/sandbox-tutorials
+
+for file in /usr/lib/tutorials/sandbox-tutorials/local/*.mp4; do 
+    mv "$file" `echo $file | sed -r -e 's/([^0-9]*).*/\1.mp4/g'`;
+done
+
 exit 0
