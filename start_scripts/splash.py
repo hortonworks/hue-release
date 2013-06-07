4857916a024030f5b89170e7d34626db999b54c8
Roman Rader <antigluk@gmail.com>
2013-06-07 18:59:56 +0300

    SSH port on splash screen

--- start_scripts/splash.py
+++ start_scripts/splash.py
@@ -52,6 +52,8 @@ def make_ip_window():
         ip_win.addstr(2, 2, "please open a browser and enter this address ")
         ip_win.addstr(3, 2, "in the browser's address field: ")
         ip_win.addstr(4, 2, "http://%s/" % ip)
+        if platform == "vbox":
+            ip_win.addstr(6, 2, "You can access SSH on %s:2222" % ip)
 
 
 def make_hint_window():
