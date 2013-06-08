624b0ed5dae95c2940c3a3e202aa8414ac0287c0
Roman Rader <antigluk@gmail.com>
2013-06-08 23:33:18 +0300

    fixed splash

--- start_scripts/splash.py
+++ start_scripts/splash.py
@@ -53,7 +53,7 @@ def make_ip_window():
         ip_win.addstr(3, 2, "in the browser's address field: ")
         ip_win.addstr(4, 2, "http://%s/" % ip)
         if platform == "vbox":
-            ip_win.addstr(6, 2, "You can access SSH on 127.0.0.1:2222" % ip)
+            ip_win.addstr(6, 2, "You can access SSH on 127.0.0.1:2222")
 
 
 def make_hint_window():
