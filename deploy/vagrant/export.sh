5078d1ad846f85d4b86aa2666c7f362a7317a5fc
Roman Rader <antigluk@gmail.com>
2013-06-09 11:41:17 +0300

    HDP -> Hortonworks

--- deploy/vagrant/export.sh
+++ deploy/vagrant/export.sh
@@ -10,7 +10,7 @@ VBoxManage sharedfolder remove "$MACHINE" --name '/vagrant'
 VBoxManage sharedfolder remove "$MACHINE" --name '/tmp/vagrant-puppet/manifests'
 echo $MACHINE
 VBoxManage modifyvm "$MACHINE" --memory 2048 --cpus 2
-VBoxManage export "$MACHINE" --output "HDP-Sandbox-1.3-VirtualBox".ova \
+VBoxManage export "$MACHINE" --output "Hortonworks-Sandbox-1.3-VirtualBox".ova \
     --vsys 0 --vendor "HortonWorks" --version "1.3" --product "Sandbox"
 
 # VMware
@@ -21,7 +21,7 @@ vagrant halt
 
 HDDFILE="$(VBoxManage list hdds | grep -e "$MACHINE/" | sed -r "s/.*:[^/]*(.*)/\1/g")"
 cp "$HDDFILE" ./vmware/sandbox.vmdk
-ovftool "./vmware/HDP Sandbox 1.3.vmx" "./HDP-Sandbox-1.3-VMware.ova"
+ovftool "./vmware/Hortonworks Sandbox 1.3.vmx" "./Hortonworks-Sandbox-1.3-VMware.ova"
 
 # Hyper-V
 vagrant up --no-provision
