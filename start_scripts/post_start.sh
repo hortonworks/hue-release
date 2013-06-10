2bac8258defbb69bf3e3e5731ecf87ba7bfdcb69
Roman Rader <antigluk@gmail.com>
2013-06-10 23:54:15 +0300

    hbase in post_start

--- start_scripts/post_start.sh
+++ start_scripts/post_start.sh
@@ -6,6 +6,8 @@ LOG="/var/log/startup_script.log"
 
 (
  nohup sudo -u hue bash /usr/lib/tutorials/tutorials_app/run/run.sh
+ su - hbase -c "/usr/lib/hbase/bin/hbase-daemon.sh --config /etc/hbase/conf start master"; sleep 5
+ su - hbase -c "/usr/lib/hbase/bin/hbase-daemon.sh --config /etc/hbase/conf start regionserver"
 )  >> $LOG 2>&1 & 
 
 python /usr/lib/hue/tools/start_scripts/splash.py
