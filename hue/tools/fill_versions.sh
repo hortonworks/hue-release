ea4d37975f7f5cf0bbdbd19190d32d51371bd13c
Roman Rader <antigluk@gmail.com>
2013-08-15 20:16:09 +0300

    adding hue build number to VERSIONS file

--- hue/tools/fill_versions.sh
+++ hue/tools/fill_versions.sh
@@ -12,14 +12,14 @@ function get_version() {
 
     if which yum &>/dev/null; then
         # RPM
-        VERSION=$(yum list $1 | grep "^$1\." | awk '{print $2}' | cut -d. -f${RANGE})
+        VERSION=$(yum list $1 | grep "^$1\." | awk '{print $2}' | cut -d. -f${RANGE} | sed "s/.[0-9]*-/:/g" | head -n 1)
     elif which zypper &>/dev/null; then
         # SLES
         VERSION=$(zypper search --match-exact -t package --details $1 | grep ' package ' | awk -F'|' '{print $4}')
-        VERSION=$(echo $VERSION | cut -d. -f${RANGE})
+        VERSION=$(echo $VERSION | cut -d. -f${RANGE}  | sed "s/.[0-9]*-/:/g" | head -n 1)
     elif which rpm &>/dev/null; then
         # No yum and zypper => naive check on installed packages (will not work on multi-node deployment)
-        VERSION=$(rpm -q --qf "%{VERSION}" $1 | cut -d. -f${RANGE})
+        VERSION=$(rpm -q --qf "%{VERSION}" $1 | cut -d. -f${RANGE} | sed "s/.[0-9]*-/:/g" | head -n 1)
     else
         # something other
         VERSION=""
@@ -28,7 +28,7 @@ function get_version() {
 }
 
 # Determining HDP stack components versions
-echo "HUE_VERSION=$(get_version hue-common)" | tee ${hue_dir}/VERSIONS
+echo "HUE_VERSION=$(get_version hue-common 1-3,7)" | tee ${hue_dir}/VERSIONS
 echo "HDP=$(get_version hadoop 4-6)" | tee -a ${hue_dir}/VERSIONS
 
 APPS="Hadoop HCatalog Pig Hive Oozie"
