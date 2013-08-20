895fac4232159c9afa8813f9370038593d5fb651
Alex Honko <ahonko@hortonworks.com>
2013-08-20 04:10:47 +0300

    Improved start/stop scripts.

--- start_scripts/stop.sh
+++ start_scripts/stop.sh
@@ -1,42 +1,59 @@
 #----- Stop script -------------
-echo "For slave node(s)"
 
-echo "Stop Hbase regionserver"
-su - hbase -c "/usr/lib/hbase/bin/hbase-daemon.sh --config /etc/hbase/conf stop regionserver"
-
-echo "Stop HDFS datanode"
-su - hdfs -c "/usr/lib/hadoop/bin/hadoop-daemon.sh --config /etc/hadoop/conf stop datanode"
-
-echo "Stop Hadoop tasktracker"
-su - mapred -c "/usr/lib/hadoop/bin/hadoop-daemon.sh --config /etc/hadoop/conf stop tasktracker"
-
-echo "Stop templeton server"
-su - hcat -c '/usr/lib/hcatalog/sbin/webhcat_server.sh stop'
-
+# host machine
 echo "Stop Oozie"
-su - oozie -c "cd /var/log/oozie; /usr/lib/oozie/bin/oozie-stop.sh"
+su -l oozie -c "cd /var/log/oozie; /usr/lib/oozie/bin/oozied.sh stop"
 
-echo "For ZooKeeper node(s)"
-echo "Stop zookeeper node"
-su - zookeeper -c  'source /etc/zookeeper/conf/zookeeper-env.sh ; /bin/env ZOOCFGDIR=/etc/zookeeper/conf ZOOCFG=zoo.cfg /usr/lib/zookeeper/bin/zkServer.sh stop'
+# host machine
+echo "Stop WebHCat"
+su -l hcat -c "/usr/lib/hcatalog/sbin/webhcat_server.sh stop"
 
-echo "For Master node(s)"
-echo "Stop the hbase master"
-su - hbase -c "/usr/lib/hbase/bin/hbase-daemon.sh --config /etc/hbase/conf stop master"
+# host machine
+echo "Stop Tez"
+su -l tez -c "/usr/lib/tez/sbin/tez-daemon.sh stop ampoolservice"
 
-echo "Stop the job tracker and history server"
-su - mapred -c "/usr/lib/hadoop/bin/hadoop-daemon.sh --config /etc/hadoop/conf stop historyserver"
-su - mapred -c "/usr/lib/hadoop/bin/hadoop-daemon.sh --config /etc/hadoop/conf stop jobtracker"
+# host machine
+echo "Stop Hive"
+ps aux | awk '{print $1,$2}' | grep hive | awk '{print $2}' | xargs kill >/dev/null 2>&1
 
-echo "Stop the secondary name node"
-su - hdfs -c "/usr/lib/hadoop/bin/hadoop-daemon.sh --config /etc/hadoop/conf stop secondarynamenode"
+# host machine
+echo "Stop ZooKeeper"
+su -l zookeeper -c "export ZOOCFGDIR=/etc/zookeeper/conf ; export ZOOCFG=zoo.cfg ;source /etc/zookeeper/conf/zookeeper-env.sh ; /usr/lib/zookeeper/bin/zkServer.sh stop"
 
-echo "Stop the name node"
-su - hdfs -c "/usr/lib/hadoop/bin/hadoop-daemon.sh --config /etc/hadoop/conf stop namenode"
+echo "Stop HBase"
+# slave node
+echo "Stop Hbase RegionServers"
+su -l hbase -c "/usr/lib/hbase/bin/hbase-daemon.sh --config /etc/hbase/conf stop regionserver"
 
-echo "Stop the hive server"
-kill `cat /var/run/hive/hive.pid` >/dev/null 2>&1
-kill `cat /var/run/hive/hive-server.pid` >/dev/null 2>&1
+# master node
+echo "Stop Hbase Master"
+su -l hbase -c "/usr/lib/hbase/bin/hbase-daemon.sh --config /etc/hbase/conf stop master"
+
+echo "Stop YARN"
+# slave node
+echo "Stop NodeManagers"
+su -l yarn -c "export HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec && /usr/lib/hadoop/sbin/hadoop-daemon.sh --config /etc/hadoop/conf stop nodemanager"
+
+# host machine
+echo "Stop JobTracker History Server"
+su -l mapred -c "export HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec && /usr/lib/hadoop/sbin/hadoop-daemon.sh --config /etc/hadoop/conf stop historyserver"
+
+# host machine
+echo "Stop ResourceManager"
+su -l yarn -c "export HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec && /usr/lib/hadoop/sbin/hadoop-daemon.sh --config /etc/hadoop/conf stop resourcemanager"
+
+echo "Stop HDFS"
+# slave node
+echo "Stop DataNodes"
+su -l hdfs -c "/usr/lib/hadoop/sbin/hadoop-daemon.sh --config /etc/hadoop/conf stop datanode"
+
+# host machine
+echo "Stop Secondary NameNode"
+su -l hdfs -c "/usr/lib/hadoop/sbin/hadoop-daemon.sh --config /etc/hadoop/conf stop secondarynamenode"
+
+# master node
+echo "Stop NameNode"
+su -l hdfs -c "/usr/lib/hadoop/sbin/hadoop-daemon.sh --config /etc/hadoop/conf stop namenode"
 
 echo "Stop MySQL server"
 /etc/init.d/mysqld stop
@@ -45,10 +62,10 @@ echo "Stop MySQL server"
 echo "Service associated with port"
 netstat -nltp
 
-echo "     			"
-echo "                          "
+echo
+echo
 echo "Java Process"
 ps auxwwwf | grep java | grep -v grep | awk '{print $1, $11,$12}'
 
-echo "======================================="
+echo
 
