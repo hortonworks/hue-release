7563d39ea16612d846eecc547de6b3eef3320715
Peter Rudenko <petro.rudenko@gmail.com>
2013-09-12 14:58:40 +0300

    BUG-8769 Can't kill oozie workflow from other user

--- hue/apps/oozie/src/oozie/views/dashboard.py
+++ hue/apps/oozie/src/oozie/views/dashboard.py
@@ -70,7 +70,9 @@ def manage_oozie_jobs(request, job_id, action):
   response = {'status': -1, 'data': ''}
 
   try:
-    response['data'] = get_oozie().job_control(job_id, action)
+    oozie_api = get_oozie()
+    oozie_api.setuser(request.user.username)
+    response['data'] = oozie_api.job_control(job_id, action)
     response['status'] = 0
     if 'notification' in request.POST:
       request.info(_(request.POST.get('notification')))
