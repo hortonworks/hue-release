c202afb19fecc543d77b687cbd05b7f48007a751
Roman Rader <antigluk@gmail.com>
2013-10-01 16:02:50 +0300

    fixed repo for condorM30-2.2.0

--- deploy/vagrant/manifests/sandbox_test_machine.pp
+++ deploy/vagrant/manifests/sandbox_test_machine.pp
@@ -3,7 +3,7 @@ import "sandbox-base.pp"
 class sandbox_rpm_dev inherits sandbox_rpm {
     File['sandbox.repo'] {
         path    => "/etc/yum.repos.d/sandbox.repo",
-        content => template("/vagrant/files/sandbox-local.repo"),
+        content => template("/vagrant/files/sandbox.repo"),
     }
 }
 
