692586487208520f978c850cfcf544d3a9e8e915
Roman Rader <rrader@hortonworks.com>
2013-11-12 17:27:10 +0200

    time sync in vmware-tools disabled (instead ntpd installed)

--- deploy/vagrant/files/scripts/vmware-tools.sh
+++ deploy/vagrant/files/scripts/vmware-tools.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+yum -y install ntp
+chkconfig ntpd on
+service ntpd stop
+ntpdate pool.ntp.org
+service ntpd start
+
 rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-DSA-KEY.pub
 rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub
 
@@ -14,3 +20,6 @@ EOF
 
 yum -y install vmware-tools-esx-kmods
 yum -y install vmware-tools-core vmware-tools-esx-nox
+
+# Workaround to avoid "Unable to collect IPv4 routing table" issue on booting in vmware
+mv /usr/lib/vmware-tools/plugins/vmsvc/libtimeSync.so /usr/lib/vmware-tools/plugins/vmsvc/libtimeSync.so.disabled
