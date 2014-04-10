7d2af6f0a246330e153700fc4d434d36222c528b
Peter Rudenko <petro.rudenko@gmail.com>
2014-04-11 00:08:08 +0300

    Minor bluprint updates

--- deploy/packer/modules/install/files/check_status.sh
+++ deploy/packer/modules/install/files/check_status.sh
@@ -2,13 +2,15 @@
 while [ 1 ]
 do
     tasks=`curl -s -u admin:admin -H 'X-Requested-By: ambari' 'http://127.0.0.1:8080/api/v1/clusters/Sandbox/requests/1?fields=tasks/Tasks/*'|python /tmp/check_status.py`
-    if [[ $? -eq 0 ]]
+    status=$?
+    if [[ $status -eq 0 ]]
     then
         exit
-    elif [[ $? -eq 2 ]]
+    elif [[ $status -eq 2 ]]
     then
         exit 1;
     else
+        echo $tasks;
         sleep 15
     fi
 done
--- deploy/packer/modules/install/manifests/ambari-bluprints.pp
+++ deploy/packer/modules/install/manifests/ambari-bluprints.pp
@@ -23,7 +23,7 @@ class install::ambari-bluprints{
   }
 
   exec {"add cluster":
-    command => "curl -f -H 'X-Requested-By: ambari' -u admin:admin http://127.0.0.1:8080/api/v1/clusters/Sandbox -d @/tmp/cluster-bluprint.json | tee /tmp/curl.log",
+    command => "curl -f -H 'X-Requested-By: ambari' -u admin:admin http://127.0.0.1:8080/api/v1/clusters/Sandbox -d @/tmp/cluster-bluprint.json",
     require => [File["/tmp/cluster-bluprint.json"],Exec["add bluprint"]],
     logoutput => true
   }
@@ -31,6 +31,7 @@ class install::ambari-bluprints{
   exec {"install cluster":
     command => "/bin/bash /tmp/check_status.sh",
     timeout => 0,
+    logoutput => true,
     require => [File["/tmp/check_status.py"], File["/tmp/check_status.sh"], Exec["add cluster"]]
   }
   
