4dc2ddd23298633eb9bf36c1ca8d385d71d170af
Alex Honko <ahonko@cybervisiontech.com>
2014-10-06 17:42:28 +0300

    BUG-23261 Beeswax/HCatalog save results data into table is broken

--- hue/apps/beeswax/src/beeswax/server/dbms.py
+++ hue/apps/beeswax/src/beeswax/server/dbms.py
@@ -253,14 +253,18 @@ class HiveServer2Dbms(object):
 
 
   def create_table_as_a_select(self, request, query_history, target_database, target_table, result_meta):
-    design = query_history.design.get_design()
-    database = design.query['database']
+    database = target_database
+    query = query_history.query
+    if query_history.design:
+      design = query_history.design.get_design()
+      database = design.query['database']
+      query = design.query['query']
 
     # Case 1: Hive Server 2 backend or results straight from an existing table
     if result_meta.in_tablename:
       self.use(database)
 
-      hql = 'CREATE TABLE %s.%s AS %s' % (target_database, target_table, design.query['query'])
+      hql = 'CREATE TABLE %s.%s AS %s' % (target_database, target_table, query)
       query_history = self.execute_statement(hql)
     else:
       # Case 2: The results are in some temporary location
