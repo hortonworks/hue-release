09c3f95f06f32272c3614d6479fcefb29ec34600
Don Bosco Durai <bdurai@hortonworks.com>
2014-10-10 19:57:54 -0700

    Fixed looking for override folder

--- deploy/packer/modules/postinstall/files/argus/scripts/argus_override_properties_and_install.sh
+++ deploy/packer/modules/postinstall/files/argus/scripts/argus_override_properties_and_install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-install_overrides_folder=$1
 
+#Make sure this script is from where folder which contains the override files
+#e.g. cd /vagrant/modules/postinstall/files/argus/install_overrides
+install_overrides_folder=`pwd`
 
+if [ ! -d admin ]; then
+	echo "ERROR: This script should be run from override folder. e.g. /vagrant/modules/postinstall/files/argus/install_overrides"
+	exit 1
+fi
 
 #latest_ver=`ls | grep -v current | sort -unr | head --lines=1`
 latest_folder=`find /usr/hdp -name argus | grep -v current | sort -unr | head --lines=1`
+
+echo "Override folder=$install_overrides_folder Install_folder=$latest_folder"
 
 cd $install_overrides_folder
 cp -r * $latest_folder
