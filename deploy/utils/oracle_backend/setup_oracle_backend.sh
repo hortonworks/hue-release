be69a6cad3dec63055d7a5c8cbeb03439044c033
Alex Honko <ahonko@cybervisiontech.com>
2014-11-19 22:06:52 +0200

    BUG-28081 Hue with Oracle as db throws ORA-00942: table or view does not exist

--- deploy/utils/oracle_backend/setup_oracle_backend.sh
+++ deploy/utils/oracle_backend/setup_oracle_backend.sh
@@ -4,13 +4,16 @@
 function usage_help {
   echo
   echo "Usage:"
-  echo "bash setup_oracle_backend.sh host_os"
+  echo "USE_DEFAULT_INSTANTCLIENT=yes bash setup_oracle_backend.sh host_os"
   echo "    where host_os is one of the following:"
   echo "        centos5.9"
   echo "        centos6.4"
   echo "        suse11"
   echo
-  echo "e.g.    bash setup_oracle_backend.sh centos6.4"
+  echo "    'USE_DEFAULT_INSTANTCLIENT=yes' means to use default ORACLE_HOME and LD_LIBRARY_PATH env variables"
+  echo "    what in turn implies oracle instantclient to be already installed on the system"
+  echo
+  echo "e.g.    USE_DEFAULT_INSTANTCLIENT=yes bash setup_oracle_backend.sh centos6.4"
 }
 
 if [ -z "$1" ]
@@ -20,56 +23,58 @@ if [ -z "$1" ]
     exit 1
 fi
 
+if [ ! $USE_DEFAULT_INSTANTCLIENT ]
+  then
+    CUR_DIR=`pwd`
+    TMP_DIR="/tmp/oracle-instantclient"
+    ORACLE_INSTANTCLIENT_LIB_DIR="/usr/lib64/oracle"
+    ORACLE_INSTANTCLIENT_LD_CONF=/etc/ld.so.conf.d/oracle-instantclient-x86_64.conf
+    # Oracle Instant Client Package tarball oracle-instantclientAMD64-10.1.0.5.0.tgz contains sources downloaded
+    # from http://www.oracle.com/technetwork/topics/linuxx86-64soft-092277.html :
+    #     instantclient-basic-linuxAMD64-10.1.0.5.0-20060519.zip (32,200,686 bytes) (cksum - 991396986)
+    #     instantclient-jdbc-linuxAMD64-10.1.0.5.0-20060519.zip (4,650,957 bytes) (cksum - 2639141761)
+    #     instantclient-sqlplus-linuxAMD64-10.1.0.5.0-20060519.zip (366,963 bytes) (cksum - 567416601)
+    #     instantclient-sdk-linuxAMD64-10.1.0.5.0-20060519.zip (294,632 bytes) (cksum - 3782887635)
+    #
+    #     to make this tarball:
+    #     tar cfvz ./oracle-instantclientAMD64-10.1.0.5.0.tgz \
+    #         ./instantclient-basic-linuxAMD64-10.1.0.5.0-20060519.zip \
+    #         ./instantclient-jdbc-linuxAMD64-10.1.0.5.0-20060519.zip \
+    #         ./instantclient-sqlplus-linuxAMD64-10.1.0.5.0-20060519.zip \
+    #         ./instantclient-sdk-linuxAMD64-10.1.0.5.0-20060519.zip
 
-CUR_DIR=`pwd`
-TMP_DIR="/tmp/oracle-instantclient"
-ORACLE_INSTANTCLIENT_LIB_DIR="/usr/lib64/oracle"
-ORACLE_INSTANTCLIENT_LD_CONF=/etc/ld.so.conf.d/oracle-instantclient-x86_64.conf
-# Oracle Instant Client Package tarball oracle-instantclientAMD64-10.1.0.5.0.tgz contains sources downloaded 
-# from http://www.oracle.com/technetwork/topics/linuxx86-64soft-092277.html :
-#     instantclient-basic-linuxAMD64-10.1.0.5.0-20060519.zip (32,200,686 bytes) (cksum - 991396986)
-#     instantclient-jdbc-linuxAMD64-10.1.0.5.0-20060519.zip (4,650,957 bytes) (cksum - 2639141761)
-#     instantclient-sqlplus-linuxAMD64-10.1.0.5.0-20060519.zip (366,963 bytes) (cksum - 567416601)
-#     instantclient-sdk-linuxAMD64-10.1.0.5.0-20060519.zip (294,632 bytes) (cksum - 3782887635)
-#
-#     to make this tarball:
-#     tar cfvz ./oracle-instantclientAMD64-10.1.0.5.0.tgz \
-#         ./instantclient-basic-linuxAMD64-10.1.0.5.0-20060519.zip \
-#         ./instantclient-jdbc-linuxAMD64-10.1.0.5.0-20060519.zip \
-#         ./instantclient-sqlplus-linuxAMD64-10.1.0.5.0-20060519.zip \
-#         ./instantclient-sdk-linuxAMD64-10.1.0.5.0-20060519.zip
+    ORA_BACKEND_TAR_LINK=http://dev2.hortonworks.com.s3.amazonaws.com/stuff/oracle-instantclientAMD64-10.1.0.5.0.tgz
+    ORA_BACKEND_TAR=$CUR_DIR/oracle-instantclientAMD64-10.1.0.5.0.tgz
 
-ORA_BACKEND_TAR_LINK=http://dev2.hortonworks.com.s3.amazonaws.com/stuff/oracle-instantclientAMD64-10.1.0.5.0.tgz
-ORA_BACKEND_TAR=$CUR_DIR/oracle-instantclientAMD64-10.1.0.5.0.tgz
+    if [ ! -f "$ORA_BACKEND_TAR" ]; then
+      wget "$ORA_BACKEND_TAR_LINK"
+      if [ ! -f "$ORA_BACKEND_TAR" ]; then
+        echo "The Oracle backend tarball does not exist at $ORA_BACKEND_TAR"
+        echo "Fix the problem and try again."
+        exit 1
+      fi
+    fi
 
-if [ ! -f "$ORA_BACKEND_TAR" ]; then
-  wget "$ORA_BACKEND_TAR_LINK"
-  if [ ! -f "$ORA_BACKEND_TAR" ]; then
-    echo "The Oracle backend tarball does not exist at $ORA_BACKEND_TAR"
-    echo "Fix the problem and try again."
-    exit 1
-  fi
+    if [ ! -d "$TMP_DIR" ]; then
+      mkdir -p "$TMP_DIR"
+    fi
+
+    tar xfvz "$ORA_BACKEND_TAR" -C "$TMP_DIR"
+    cd "$TMP_DIR"
+    unzip ./instantclient-sdk-linuxAMD64-10.1.0.5.0-20060519.zip
+    unzip ./instantclient-sqlplus-linuxAMD64-10.1.0.5.0-20060519.zip
+    unzip ./instantclient-basic-linuxAMD64-10.1.0.5.0-20060519.zip
+    unzip ./instantclient-jdbc-linuxAMD64-10.1.0.5.0-20060519.zip
+
+    mkdir -p $ORACLE_INSTANTCLIENT_LIB_DIR
+    cp -f $TMP_DIR/instantclient10_1/*.so* $ORACLE_INSTANTCLIENT_LIB_DIR
+    ln -s $ORACLE_INSTANTCLIENT_LIB_DIR/libclntsh.so.10.1 $ORACLE_INSTANTCLIENT_LIB_DIR/libclntsh.so
+    ln -s $ORACLE_INSTANTCLIENT_LIB_DIR/libclntsh.so.10.1 /usr/lib/libclntsh.so
+    echo $ORACLE_INSTANTCLIENT_LIB_DIR > $ORACLE_INSTANTCLIENT_LD_CONF
+    ldconfig
+    export ORACLE_HOME="$TMP_DIR/instantclient10_1"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$ORACLE_HOME"
 fi
-
-if [ ! -d "$TMP_DIR" ]; then
-  mkdir -p "$TMP_DIR"
-fi
-
-tar xfvz "$ORA_BACKEND_TAR" -C "$TMP_DIR"
-cd "$TMP_DIR"
-unzip ./instantclient-sdk-linuxAMD64-10.1.0.5.0-20060519.zip
-unzip ./instantclient-sqlplus-linuxAMD64-10.1.0.5.0-20060519.zip
-unzip ./instantclient-basic-linuxAMD64-10.1.0.5.0-20060519.zip
-unzip ./instantclient-jdbc-linuxAMD64-10.1.0.5.0-20060519.zip
-
-mkdir -p $ORACLE_INSTANTCLIENT_LIB_DIR
-cp -f $TMP_DIR/instantclient10_1/*.so* $ORACLE_INSTANTCLIENT_LIB_DIR
-ln -s $ORACLE_INSTANTCLIENT_LIB_DIR/libclntsh.so.10.1 $ORACLE_INSTANTCLIENT_LIB_DIR/libclntsh.so
-ln -s $ORACLE_INSTANTCLIENT_LIB_DIR/libclntsh.so.10.1 /usr/lib/libclntsh.so
-echo $ORACLE_INSTANTCLIENT_LIB_DIR > $ORACLE_INSTANTCLIENT_LD_CONF
-ldconfig
-export ORACLE_HOME="$TMP_DIR/instantclient10_1"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$ORACLE_HOME"
 
 case "$1" in
     "centos5.9" )
@@ -94,7 +99,7 @@ esac
 cd /usr/lib/hue/
 . ./build/env/bin/activate
 pip install south --upgrade
-pip install cx_Oracle
+pip install --upgrade cx_Oracle
 hue syncdb --noinput
 hue migrate
 deactivate
