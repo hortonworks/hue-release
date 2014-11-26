487b223a93f3aea8205f1364a552f32ae81a6c53
Alex Honko <ahonko@cybervisiontech.com>
2014-11-26 12:20:24 +0200

    Updated sandbox tutorial branch to HDP2.2.

--- start_scripts/consts.sh
+++ start_scripts/consts.sh
@@ -5,6 +5,6 @@ export TUTORIALS_HOME=/usr/lib/tutorials
 export GIT_FILES=$TUTORIALS_HOME/sandbox-tutorials
 
 export BRANCH=master
-export TUTORIALS_BRANCH=Champlain-preview
+export TUTORIALS_BRANCH=HDP2.2
 
 export HUE_HOME_DIR=/usr/lib/hue
