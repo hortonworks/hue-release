1f2017d7b581a6005d6036e203c320eb2273dabe
Peter Rudenko <petro.rudenko@gmail.com>
2014-12-03 14:44:46 +0200

    Ambari 1.7 GA

--- deploy/packer/modules/install/manifests/ambari-agent.pp
+++ deploy/packer/modules/install/manifests/ambari-agent.pp
@@ -1,7 +1,7 @@
 class install::ambari-agent {
 
   exec{"ambari-repo":
-    command => "wget -O /etc/yum.repos.d/ambari.repo http://public-repo-1.hortonworks.com.s3.amazonaws.com/HDP/centos6/2.x/GA/2.2.0.0/hdp.repo",
+    command => "wget -O /etc/yum.repos.d/ambari.repo http://public-repo-1.hortonworks.com.s3.amazonaws.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo",
     unless => "ls -al /etc/yum.repos.d/ambari.repo"
   }
 
