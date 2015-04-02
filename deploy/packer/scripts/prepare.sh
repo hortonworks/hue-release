88f197ddca9f66fe2c68e4c5ed441fb3ac9e48e7
paul-lupu <lupugeorgepaul@gmail.com>
2015-04-02 18:25:45 +0300

    install hieradata database on guest

--- deploy/packer/scripts/prepare.sh
+++ deploy/packer/scripts/prepare.sh
@@ -1,6 +1,8 @@
 yum update -y
 yum install -y yum-utils wget redhat-lsb
 mkdir /tmp/install
+echo "installing hieradata files"
+mkdir /vagrant
 chmod 700 /tmp/install
 printf 'To login to the the shell, use:\nusername: root\npassword: hadoop\n' >> /etc/issue
 reboot && sleep 150
