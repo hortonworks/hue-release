d605ca5390a8537474dff15c6b4f93d1096c786c
paul-lupu <lupugeorgepaul@gmail.com>
2015-04-02 19:57:05 +0300

    2nd fix for packer permission issues

--- deploy/packer/scripts/prepare.sh
+++ deploy/packer/scripts/prepare.sh
@@ -3,6 +3,7 @@ yum install -y yum-utils wget redhat-lsb
 mkdir /tmp/install
 echo "installing hieradata files"
 mkdir /vagrant
+chmod 777 /vagrant
 mkdir /etc/puppet
 chmod 700 /tmp/install
 printf 'To login to the the shell, use:\nusername: root\npassword: hadoop\n' >> /etc/issue
