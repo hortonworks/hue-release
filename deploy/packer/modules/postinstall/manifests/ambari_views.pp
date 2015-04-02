4aaaa788b14523f397d1aa87931229c5a5b895af
paul-lupu <lupugeorgepaul@gmail.com>
2015-04-02 15:39:07 +0300

    added pig, hive, files and cs views to 2.2-maint

--- deploy/packer/modules/postinstall/manifests/ambari_views.pp
+++ deploy/packer/modules/postinstall/manifests/ambari_views.pp
@@ -1,152 +1,201 @@
 class postinstall::ambari_views{
-  class download_ambari_views {
-
-/*
+  include download_ambari_views
+  include create_instances
+  include create_users
+  include finish_install
+  
+  Class['download_ambari_views']->
+  Class['create_instances']->
+  Class['create_users']->
+  Class['finish_install']
+}
+  class download_ambari_views{
+    
     exec {"files":
-      command => "wget -O /var/lib/ambari-server/resources/views/files-0.1.0-SNAPSHOT.jar http://dev2.hortonworks.com.s3.amazonaws.com/ARTIFACTS/views/files-0.1.0-SNAPSHOT.jar",
+      command => "/usr/bin/wget --directory-prefix='/var/lib/ambari-server/resources/views/' $files_jar",
       timeout => 0,
-      creates => "/var/lib/ambari-server/resources/views/files-0.1.0-SNAPSHOT.jar"
     }
 
     exec {"pig":
-      command => "wget -O /var/lib/ambari-server/resources/views/pig-0.1.0-SNAPSHOT.jar http://dev2.hortonworks.com.s3.amazonaws.com/ARTIFACTS/views/pig-0.1.0-SNAPSHOT.jar",
+      command => "/usr/bin/wget --directory-prefix='/var/lib/ambari-server/resources/views/' $pig_jar",
       timeout => 0,
-      creates => "/var/lib/ambari-server/resources/views/pig-0.1.0-SNAPSHOT.jar"
     }
 
     exec {"capacity-scheduler":
-      command => "wget -O /var/lib/ambari-server/resources/views/capacity-scheduler-0.0.1-SNAPSHOT.jar http://dev2.hortonworks.com.s3.amazonaws.com/ARTIFACTS/views/capacity-scheduler-0.0.1-SNAPSHOT.jar",
+      command => "/usr/bin/wget --directory-prefix='/var/lib/ambari-server/resources/views/' $cs_jar",
       timeout => 0,
-      creates => "/var/lib/ambari-server/resources/views/capacity-scheduler-0.0.1-SNAPSHOT.jar"
     }
-
+    exec {"hive":
+      command => "/usr/bin/wget --directory-prefix='/var/lib/ambari-server/resources/views/' $hive_jar",
+      timeout => 0,
+      }
     file { 'pig-view-props.json':
-      path    => "/tmp/pig-view-props.json",
-      content => '[ {
-"ViewInstanceInfo" : {
-"properties" : {
-"dataworker.webhcat.url" : "http://sandbox.hortonworks.com:50111/templeton/v1",
-"dataworker.webhcat.user" : "hue",
-"dataworker.scripts.path" : "/tmp/.pigscripts",
-"dataworker.jobs.path" : "/tmp/.pigjobs",
-"dataworker.defaultFs" : "webhdfs://sandbox.hortonworks.com:50070",
-"dataworker.username" : "pigview"
-}
-}
-} ]'
+        path    => "/tmp/pig-view-props.json",
+        source => "puppet:///modules/postinstall/ambari_views/pig-view-props.json"
     }
 
     file { 'files-view-props.json':
-      path    => "/tmp/files-view-props.json",
-      content => '
-[ {
-"ViewInstanceInfo" : {
-"properties" : {
-"dataworker.defaultFs" : "hdfs://sandbox.hortonworks.com:8020"
-}
-}
-} ]'
+        path    => "/tmp/files-view-props.json",
+        source => "puppet:///modules/postinstall/ambari_views/files-view-props.json"
     }
-
+    file { 'hive-view-props.json':
+        path  =>  "/tmp/hive-view-props.json",
+        source => "puppet:///modules/postinstall/ambari_views/hive-view-props.json"
+    }
     file { 'capsched-view-props.json':
-      path    => "/tmp/capsched-view-props.json",
-      content => '[ {
-"ViewInstanceInfo" : {
-"properties" : {
-"ambari.server.url" : "http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox",
-"ambari.server.username" : "admin",
-"ambari.server.password" : "admin"
-}
-}
-} ]'
+        path    => "/tmp/capsched-view-props.json",
+        source => "puppet:///modules/postinstall/ambari_views/capsched-view-props.json"
     }
-*/
-
     file { 'slider-view-props.json':
-      path    => "/tmp/slider-view-props.json",
-      content => '[ {
-"ViewInstanceInfo" : {
-"properties" : {
-"ambari.server.url" : "http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox",
-"ambari.server.username" : "admin",
-"ambari.server.password" : "admin"
-}
-}
-} ]'
+        path    => "/tmp/slider-view-props.json",
+        source => "puppet:///modules/postinstall/ambari_views/slider-view-props.json"
     }
-  }
+### will add users now to views
+    file {'users.json':
+        path  =>  "/tmp/users.json",
+        source => "puppet:///modules/postinstall/ambari_views/users.json"
+    }
+    file {'groups.json':
+        path  => "/tmp/groups.json",
+        source => "puppet:///modules/postinstall/ambari_views/groups.json"
+    }
+    file {'adduserstogroup.json':
+      path  => "/tmp/adduserstogroup.json",
+      source => "puppet:///modules/postinstall/ambari_views/adduserstogroup.json"
+    }
+    file {'addgrouptoview.json':
+      path  => "/tmp/addgrouptoview.json",
+      source  => 'puppet:///modules/postinstall/ambari_views/addgrouptoview.json'
+    }
+}
 
-  class create_instances {
-    include download_ambari_views
+class create_instances{
 
-    exec {'install_views':
-      command => 'ambari-server restart; while ! curl -s --user admin:admin 127.0.0.1:8080/api/v1/views 2>&1 >/dev/null; do sleep 1; done',
+  exec {'install_views':
+      command => '/usr/sbin/ambari-server restart; while ! curl -s --user admin:admin 127.0.0.1:8080/api/v1/views 2>&1 >/dev/null; do sleep 1; done',
       require => Class[download_ambari_views],
     }
 
-/*
-    exec {'files_wait_deploy':
+
+  exec {'files_wait_deploy':
       command => 'while curl -s --user admin:admin http://127.0.0.1:8080/api/v1/views/FILES/versions/0.1.0 | grep DEPLOYING >/dev/null; do sleep 1; done',
       require => Exec['install_views'],
       provider => 'shell',
       timeout => 0,
     }
 
-    exec {'files_instance':
+  exec {'files_instance':
       command => 'curl -v -X POST --user admin:admin -H X-Requested-By:ambari 127.0.0.1:8080/api/v1/views/FILES/versions/0.1.0/instances/MyFiles --data "@/tmp/files-view-props.json"',
       require => Exec['files_wait_deploy'],
       provider => 'shell',
       timeout => 0,
     }
 
-    exec {'pig_wait_deploy':
+  exec {'pig_wait_deploy':
       command => 'while curl -s --user admin:admin http://127.0.0.1:8080/api/v1/views/PIG/versions/0.1.0 | grep DEPLOYING >/dev/null; do sleep 1; done',
       require => Exec['install_views'],
       provider => 'shell',
       timeout => 0,
     }
 
-    exec {'pig_instance':
+  exec {'pig_instance':
       command => 'curl -v -X POST --user admin:admin -H X-Requested-By:ambari 127.0.0.1:8080/api/v1/views/PIG/versions/0.1.0/instances/MyPig --data "@/tmp/pig-view-props.json"',
       require => Exec['pig_wait_deploy'],
       provider => 'shell',
       timeout => 0,
     }
 
-    exec {'capsched_wait_deploy':
+  exec {'capsched_wait_deploy':
       command => 'while curl -s --user admin:admin http://127.0.0.1:8080/api/v1/views/CAPACITY-SCHEDULER/versions/0.1.0 | grep DEPLOYING >/dev/null; do sleep 1; done',
       require => Exec['install_views'],
       provider => 'shell',
       timeout => 0,
     }
 
-    exec {'capsched_instance':
-      command => 'curl -v -X POST --user admin:admin -H X-Requested-By:ambari 127.0.0.1:8080/api/v1/views/CAPACITY-SCHEDULER/versions/0.1.0/instances/CS_1 --data "@/tmp/capsched-view-props.json"',
+  exec {'capsched_instance':
+      command => 'curl -v -X POST --user admin:admin -H X-Requested-By:ambari 127.0.0.1:8080/api/v1/views/CAPACITY-SCHEDULER/versions/0.3.0/instances/CS_1 --data "@/tmp/capsched-view-props.json"',
       require => Exec['capsched_wait_deploy'],
       provider => 'shell',
       timeout => 0,
     }
-*/
-    exec {'slider_wait_deploy':
-      command => 'while curl -s --user admin:admin http://127.0.0.1:8080/api/v1/views/SLIDER/versions/1.0.0 | grep DEPLOYING >/dev/null; do sleep 1; done',
+  exec {'hive_wait_deploy':
+      command => 'while curl -s --user admin:admin http://127.0.0.1:8080/api/v1/views/HIVE/versions/0.0.1 | grep DEPLOYING >/dev/null; do sleep 1; done',
       require => Exec['install_views'],
       provider => 'shell',
       timeout => 0,
-    }
+  }
 
-
-    exec {'slider_instance':
-      command => 'curl -v -X POST --user admin:admin -H X-Requested-By:ambari 127.0.0.1:8080/api/v1/views/SLIDER/versions/1.0.0/instances/SLIDER_1 --data "@/tmp/slider-view-props.json"',
-      require => Exec['slider_wait_deploy'],
+  exec {'hive_instance':
+      command => 'curl -v -X POST --user admin:admin -H X-Requested-By:ambari 127.0.0.1:8080/api/v1/views/HIVE/versions/0.0.1/instances/MyHive --data "@/tmp/hive-view-props.json"',
+      require => Exec['hive_wait_deploy'],
       provider => 'shell',
       timeout => 0,
-    }
-  }
-  include create_instances
-
-  exec {'finish_views_install':
-    command => 'ambari-server restart; while ! curl -s --user admin:admin 127.0.0.1:8080/api/v1/views 2>&1 >/dev/null; do sleep 1; done',
-    require => Class[create_instances],
   }
 
+
+  exec {'slider_wait_deploy':
+    command => 'while curl -s --user admin:admin http://127.0.0.1:8080/api/v1/views/SLIDER/versions/1.0.0 | grep DEPLOYING >/dev/null; do sleep 1; done',
+    require => Exec['install_views'],
+    provider => 'shell',
+    timeout => 0,
+  }
+
+
+  exec {'slider_instance':
+    command => 'curl -v -X POST --user admin:admin -H X-Requested-By:ambari 127.0.0.1:8080/api/v1/views/SLIDER/versions/1.0.0/instances/SLIDER_1 --data "@/tmp/slider-view-props.json"',
+    require => Exec['slider_wait_deploy'],
+    provider => 'shell',
+    timeout => 0,
+  }
 }
+
+class  create_users{
+  exec {'create_users':
+    command => 'curl -H "X-Requested-By: ambari" -X POST -u admin:admin http://localhost:8080/api/v1/users -d @/tmp/users.json',
+    provider => 'shell',
+    require => Class['create_instances'],
+  }
+  exec {'create_group':
+    command => 'curl -v -H "X-Requested-By: ambari" -X POST -u admin:admin http://localhost:8080/api/v1/groups -d @/tmp/groups.json',
+    require => Exec['create_users'],
+    provider => 'shell',
+  }
+  exec {'add_users_to_group':
+    command => 'curl -v -H "X-Requested-By: ambari" -X PUT -u admin:admin http://localhost:8080/api/v1/groups/views/members -d @/tmp/adduserstogroup.json',
+    require => Exec['create_group'],
+    provider => 'shell',
+  }
+  exec {'add_group_to_CS':
+    command => 'curl -v -H "X-Requested-By: ambari" -X PUT -u admin:admin http://localhost:8080/api/v1/views/CAPACITY-SCHEDULER/versions/0.3.0/instances/CS_1/privileges -d @/tmp/addgrouptoview.json',
+    require => Exec['add_users_to_group'],
+    provider => 'shell',
+  }
+  exec {'add_group_to_files':
+    command => 'curl -v -H "X-Requested-By: ambari" -X PUT -u admin:admin http://localhost:8080/api/v1/views/FILES/versions/0.1.0/instances/MyFiles/privileges -d @/tmp/addgrouptoview.json',
+    require => Exec['add_users_to_group'],
+    provider => 'shell',
+  }
+  exec {'add_group_to_hive':
+    command => 'curl -v -H "X-Requested-By: ambari" -X PUT -u admin:admin http://localhost:8080/api/v1/views/HIVE/versions/0.0.1/instances/MyHive/privileges -d @/tmp/addgrouptoview.json',
+    require => Exec['add_users_to_group'],
+    provider => 'shell',
+  }
+  exec {'add_group_to_pig':
+    command => 'curl -v -H "X-Requested-By: ambari" -X PUT -u admin:admin http://localhost:8080/api/v1/views/PIG/versions/0.1.0/instances/MyPig/privileges -d @/tmp/addgrouptoview.json',
+    require => Exec['add_users_to_group'],
+    provider => 'shell',
+  }
+  exec {'add_group_to_slider':
+    command => 'curl -v -H "X-Requested-By: ambari" -X PUT -u admin:admin http://localhost:8080/api/v1/views/SLIDER/versions/1.0.0/instances/SLIDER_1/privileges -d @/tmp/addgrouptoview.json',
+    require => Exec['add_users_to_group'],
+    provider => 'shell',
+  }
+}
+
+class finish_install{
+ exec {'finish_views_install':
+    command => '/usr/sbin/ambari-server restart; while ! curl -s --user admin:admin 127.0.0.1:8080/api/v1/views 2>&1 >/dev/null; do sleep 1; done',
+    require => Class[create_instances]
+  }
+}
+include postinstall::ambari_views
