b23ea3d3daf44d0fb5ebe081305d6259c4c339fa
paul-lupu <lupugeorgepaul@gmail.com>
2015-04-10 17:19:29 +0300

    updated timeouts for solr

--- deploy/packer/modules/postinstall/manifests/solr.pp
+++ deploy/packer/modules/postinstall/manifests/solr.pp
@@ -21,22 +21,22 @@ class postinstall::solr{
     command => "wget -N -P /var/cache/wget http://mirror.reverse.net/pub/apache/lucene/solr/4.10.4/solr-4.10.4.tgz && cp /var/cache/wget/solr*.tgz ./solr.tar.gz && tar xvf solr.tar.gz && rm solr.tar.gz",
     cwd => "/opt/solr",
     require => [File["/opt/solr"]],
-    timeout => 0,
-    creates => "/opt/solr/solr-4.7.2"
+    timeout => 200,
+    creates => "/opt/solr/solr-4.10.4"
   }
 
   exec {"download lucidworks-hadoop":
     command => "wget -N -P /var/cache/wget http://dev2.hortonworks.com.s3.amazonaws.com/stuff/lucidworks-hadoop-lws-job-1.3.0.jar && cp /var/cache/wget/lucidworks-hadoop-lws-job-1.3.0.jar ./",
     cwd => "/opt/solr",
     require => [File["/opt/solr"]],
-    timeout => 0,
+    timeout => 200,
     creates => "/opt/solr/lucidworks-hadoop-lws-job-1.3.0.jar"
   }
 
   exec {"prepare_solr":
     command => "/bin/bash /tmp/install/solr_prepare.sh",
     require => [File["/tmp/install/solr_prepare.sh"],Exec["download solr"],Exec["download lucidworks-hadoop"]],  # ,Class["install::ambari-bluprints"]
-    timeout => 0
+    timeout => 200
   }
 
   
