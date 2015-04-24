0c6a6471e24f17e5b860188b4a30d89e005e2c16
paul-lupu <lupugeorgepaul@gmail.com>
2015-04-24 18:19:25 +0300

    added timeout for stale hive restart

--- deploy/packer/modules/postinstall/manifests/patch.pp
+++ deploy/packer/modules/postinstall/manifests/patch.pp
@@ -17,15 +17,21 @@ class postinstall::patch{
 		command	=> "cp /usr/share/java/mysql-connector-java.jar /usr/hdp/*/knox/ext/",
 		provider	=> 'shell',
 	}
-	file{'stale_hive':
-		path	=> '/tmp/stale_hive.json',
-		source	=> 'puppet:///modules/postinstall/patch/stale_hive.json'
-		
-	}
-	exec{'stale_hive_restart':
-		require	=> File['stale_hive'],
-		provider	=> 'shell',
-		command		=> 'curl -i --header "Accept:application/json" -H "Content-Type: application/json" -X POST -u admin:admin http://127.0.0.1:8080/api/v1/clusters/Sandbox/requests -d @/tmp/stale_hive.json'
-	}
+ ambariApi {"restart_stale_ambari":
+    url => "services",
+    method => "POST",
+    body => '{"RequestInfo":{"command":"RESTART","context":"Restart all components with Stale Configs for HIVE","operation_level":{"level":"SERVICE","cluster_name":"Sandbox","service_name":"HIVE"}},"Requests/resource_filters":[{"service_name":"HIVE","component_name":"WEBHCAT_SERVER","hosts":"sandbox.hortonworks.com"}]}',
+  }
+
+#	file{'stale_hive':
+#		path	=> '/tmp/stale_hive.json',
+#		source	=> 'puppet:///modules/postinstall/patch/stale_hive.json'
+#		
+#	}
+#	exec{'stale_hive_restart':
+#		require	=> File['stale_hive'],
+#		provider	=> 'shell',
+#		command		=> 'curl -i --header "Accept:application/json" -H "Content-Type: application/json" -X POST -u admin:admin http://127.0.0.1:8080/api/v1/clusters/Sandbox/requests -d @/tmp/stale_hive.json'
+#	}
 }
 #include postinstall::patch
